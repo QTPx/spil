ac9b6a1c3fc44c537d843cd2f5f4c31393f4516a
Henrik Laurits Pedersen <henr985a@edu.eal.dk>
2017-10-05 12:58:52 +0200

    Merched fra facebook

--- spil/BattleShipMenu.cs
+++ spil/BattleShipMenu.cs
@@ -1,53 +1,59 @@
 ﻿using System;
-
 namespace spil
 {
-    public class BattleShipMenu
+    internal class BattleShipMenu
     {
-        BattleShipMenu battleShipMenu { get; set; }
-        public void Show()
+        BattleShip battleShip { get; set; }
+        public void show()
         {
-            int menuChoice = 0;
-            string input = "";
+            bool running = true;
+            string choice = "";
             do
             {
-                Console.Clear();
-                Console.WriteLine("tic tac toe");
-                Console.WriteLine();
-                Console.WriteLine("1. Opret nyt spil");
-
-                Console.WriteLine("0. exit");
-
-                Console.Write("Indtast dit valg: ");
-                input = Console.ReadLine();
-                int.TryParse(input, out menuChoice);
-
-                switch (menuChoice)
+                ShowMenu();
+                choice = GetUserChoise();
+                switch (choice)
                 {
-                    case 0:
-                        break;
-                    case 1:
-                        ExecuteMenuChoice1();
-                        break;
-                    case 2:
-                        ExecuteMenuChoice1();
-                        break;
-                    default:
-                        Console.WriteLine("Ugyldigt valg.");
-                        Console.ReadLine();
-                        break;
+                    case "1": DoActionFor1(); break;
+                    case "2": DoActionFor2(); break;
+                    case "0": running = false; break;
+                    default: ShowMenuSelectionError(); break;
                 }
-
-            } while (menuChoice > 0);
-
+            } while (running);
+        }
+        private void ShowMenu()
+        {
+            Console.Clear();
+            if (battleShip != null)
+            {
+                Console.WriteLine(battleShip.GetGameBoardView());
+            }
+            Console.WriteLine("Sænke Slagskip");
+            Console.WriteLine();
+            Console.WriteLine("1. Opret nyt spil");
+            Console.WriteLine("2. Set en brik");
+            Console.WriteLine();
+            Console.WriteLine("0. exit");
+        }
+        private string GetUserChoise()
+        {
+            Console.WriteLine();
+            Console.Write("Indtast dit valg: ");
+            return Console.ReadLine();
         }
 
-        private void ExecuteMenuChoice1()
+        private void ShowMenuSelectionError()
         {
-            battleShipMenu = new BattleShipMenu();
-            {
+            Console.WriteLine("Ugyldigt valg.");
+            Console.ReadLine();
+        }
 
-            }
+        private void DoActionFor1()
+        {
+            battleShip = new BattleShip();
+        }
+        private void DoActionFor2()
+        {
 
         }
     }
