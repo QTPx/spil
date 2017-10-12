8b40846cfa88b5e39c5e3767aaaf7aba5abdbcb6
Henrik Laurits Pedersen <henr985a@edu.eal.dk>
2017-10-12 13:57:44 +0200

    updated gameplade

--- spil/Ship.cs
+++ spil/Ship.cs
@@ -5,124 +5,124 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace spil
-{ 
-public class Ship
 {
-       
-public Ship()
-{
-List<Ship> lstShips = new List<Ship>();
-
-Ship s = new Ship()
-{
-    Name = "Battleship",
-    Orientation = ShipOrientation.Horizontal,
-    ExtentUnits = 4,
-    Position_Row = x - 1,
-    Position_Col = y - 1
-};
-lstShips.Add(s);
-
-s = new Ship()
-{
-    Name = "Battleship",
-    Orientation = ShipOrientation.Vertical,
-    ExtentUnits = 4,
-    Position_Row = x - 1,
-    Position_Col = y - 1
-};
-    lstShips.Add(s);
-
-    s = new Ship()
+    public class Ship
     {
-    Name = "Destroyer",
-Orientation = ShipOrientation.Horizontal,
-ExtentUnits = 3,
-        Position_Row = x - 1,
-        Position_Col = y - 1
-    };
-lstShips.Add(s);
 
-s = new Ship()
-{
-    Name = "Destroyer",
-    Orientation = ShipOrientation.Vertical,
-    ExtentUnits = 3,
-    Position_Row = x - 1,
-    Position_Col = y - 1
-};
-    lstShips.Add(s);
+        public Ship()
+        {
+            List<Ship> lstShips = new List<Ship>();
 
-    s = new Ship()
-    {
-    Name = "Submarine",
-    Orientation = ShipOrientation.Vertical,
-    ExtentUnits = 3,
-        Position_Row = x - 1,
-        Position_Col = y - 1
-    };
-    lstShips.Add(s);
+            Ship s = new Ship()
+            {
+                Name = "Battleship",
+                Orientation = ShipOrientation.Horizontal,
+                ExtentUnits = 4,
+                Position_Row = x - 1,
+                Position_Col = y - 1
+            };
+            lstShips.Add(s);
 
-    s = new Ship()
-    {
-    Name = "Submarine",
-    Orientation = ShipOrientation.Horizontal,
-    ExtentUnits = 3,
-    Position_Row = x - 1,
-    Position_Col = y - 1
-    };
-    lstShips.Add(s);
+            s = new Ship()
+            {
+                Name = "Battleship",
+                Orientation = ShipOrientation.Vertical,
+                ExtentUnits = 4,
+                Position_Row = x - 1,
+                Position_Col = y - 1
+            };
+            lstShips.Add(s);
 
-    s = new Ship()
-    {
-    Name = "Carrier",
-    Orientation = ShipOrientation.Horizontal,
-    ExtentUnits = 5,
-    Position_Row = x - 1,
-    Position_Col = y - 1
-    };
-    lstShips.Add(s);
+            s = new Ship()
+            {
+                Name = "Destroyer",
+                Orientation = ShipOrientation.Horizontal,
+                ExtentUnits = 3,
+                Position_Row = x - 1,
+                Position_Col = y - 1
+            };
+            lstShips.Add(s);
 
-    s = new Ship()
-    {
-    Name = "Carrier",
-    Orientation = ShipOrientation.Vertical,
-    ExtentUnits = 5,
-    Position_Row = x - 1,
-    Position_Col = y - 1
-    };
-    lstShips.Add(s);
+            s = new Ship()
+            {
+                Name = "Destroyer",
+                Orientation = ShipOrientation.Vertical,
+                ExtentUnits = 3,
+                Position_Row = x - 1,
+                Position_Col = y - 1
+            };
+            lstShips.Add(s);
 
-    s = new Ship()
-    {
-    Name = "Patrol Ship",
-    Orientation = ShipOrientation.Vertical,
-    ExtentUnits = 2,
-    Position_Row = x - 1,
-    Position_Col = y - 1
-    };
-    lstShips.Add(s);
+            s = new Ship()
+            {
+                Name = "Submarine",
+                Orientation = ShipOrientation.Vertical,
+                ExtentUnits = 3,
+                Position_Row = x - 1,
+                Position_Col = y - 1
+            };
+            lstShips.Add(s);
 
-    s = new Ship()
-    {
-    Name = "Patrol Ship",
-    Orientation = ShipOrientation.Horizontal,
-    ExtentUnits = 2,
-    Position_Row = x - 1,
-    Position_Col = y - 1
-    };
-    lstShips.Add(s);
+            s = new Ship()
+            {
+                Name = "Submarine",
+                Orientation = ShipOrientation.Horizontal,
+                ExtentUnits = 3,
+                Position_Row = x - 1,
+                Position_Col = y - 1
+            };
+            lstShips.Add(s);
+
+            s = new Ship()
+            {
+                Name = "Carrier",
+                Orientation = ShipOrientation.Horizontal,
+                ExtentUnits = 5,
+                Position_Row = x - 1,
+                Position_Col = y - 1
+            };
+            lstShips.Add(s);
+
+            s = new Ship()
+            {
+                Name = "Carrier",
+                Orientation = ShipOrientation.Vertical,
+                ExtentUnits = 5,
+                Position_Row = x - 1,
+                Position_Col = y - 1
+            };
+            lstShips.Add(s);
+
+            s = new Ship()
+            {
+                Name = "Patrol Ship",
+                Orientation = ShipOrientation.Vertical,
+                ExtentUnits = 2,
+                Position_Row = x - 1,
+                Position_Col = y - 1
+            };
+            lstShips.Add(s);
+
+            s = new Ship()
+            {
+                Name = "Patrol Ship",
+                Orientation = ShipOrientation.Horizontal,
+                ExtentUnits = 2,
+                Position_Row = x - 1,
+                Position_Col = y - 1
+            };
+            lstShips.Add(s);
+        }
+
+
+
+
+        public string Name { get; private set; }
+        public object Orientation { get; private set; }
+        public int ExtentUnits { get; private set; }
+        public int Position_Row { get; private set; }
+        public int Position_Col { get; private set; }
+        public object ShipOrientation { get; private set; }
+        public object Horizontal { get; set; }
     }
-
-
-
-        
-public string Name { get; private set; }
-public object Orientation { get; private set; }
-public int ExtentUnits { get; private set; }
-public int Position_Row { get; private set; }
-public int Position_Col { get; private set; }
-public object ShipOrientation { get; private set; }
-public object Horizontal { get; set; VerticalContentAlignment; }
-}
 }
